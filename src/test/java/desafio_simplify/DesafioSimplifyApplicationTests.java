a441f3bf737c11274b40eb659770d591602b5301
eliasfernandescout <eliasfernandescout@gmail.com>
2024-10-27 12:52:14 -0300

    first commit

---
+++ src/test/java/desafio_simplify/DesafioSimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio_simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
